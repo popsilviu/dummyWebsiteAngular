7c7f4fdc4fa6c2e107becc8125ef3fffaea8b63c
Pop Silviu <1629135+popsilviu@users.noreply.github.com>
2023-05-25 20:12:09 +0300

    Added routing to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'home',
+  component: ProductListComponent
+},
+{
+  path: 'shopping-cart',
+  component: ShoppingCartComponent
+},
+{
+  path: '**',
+  redirectTo: '/home'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
